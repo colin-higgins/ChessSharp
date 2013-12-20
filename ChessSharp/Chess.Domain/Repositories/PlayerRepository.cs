62bbf20f7d79050f5571770378389c142d27f543
SpykeBytes <DeveloperSpike@gmail.com>
2013-12-19 23:56:42 -0500

    stub player repository

---
+++ ChessSharp/Chess.Domain/Repositories/PlayerRepository.cs
@@ -0,0 +1,16 @@
+﻿using Chess.Data;
+
+namespace Chess.Domain.Repositories
+{
+    public class PlayerRepository
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PlayerRepository(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
+    }
+}
