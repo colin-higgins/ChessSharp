960f72b4c8b14dbf8c4e4320b87a1bf02366f17b
SpykeBytes <colin@spykebytes.me>
2014-01-22 22:36:44 -0500

    Fixed some broke, but code first is angry about namespaces I think

--- ChessSharp/Chess.Data/ChessContext.cs
+++ ChessSharp/Chess.Data/ChessContext.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using Chess.Data;
+using Chess.Data.Entities;
 using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Chess.Data
